a33b6ad0f810ea3c951b78d7863646bfc5d6ccac
Sandro Steeger <sandro.steeger@student.hpi.de>
2021-04-21 19:55:09 +0200

    Added documentation

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -14,26 +14,37 @@ public class Grid {
     Grid(int width, int height){
         this.width = width;
         this.height = height;
-        grid = makeGrid(width, height);
+        makeGrid(width, height);
     }
 
-    public Cell[][] makeGrid(int width, int height){
+    /**
+     * Makes a grid from the given dimensions and saves it in the class variable "grid".
+     * @param width Describes the x-dimension of the resulting grid. Must be positive.
+     * @param height Describes the y-dimension of the resulting grid. Must be positive.
+     */
+    public void makeGrid(int width, int height){
+        if(width == 0 | height == 0){
+            throw new IllegalArgumentException("width and height always have to be positive.");
+        }
+
         grid = new Cell[width][];
 
         for(int i = 0; i < width; i++){
             grid[i] = new Cell[height];
 
             for(int j = 0; j < height; j++){
-                grid[i][j] = new Cell(false);
+                grid[i][j] = new Cell(true);
             }
         }
-
-        return grid;
     }
 
+    /**
+     * Prints the grid saved in the class variable while also displaying a row and column counter.
+     * Cells that are alive will be denoted "✓" and cells that are dead will be shown as "🞩".
+     */
     public void printGrid(){
-        System.out.print("\t");
-        for(int i = 0; i < width; i++){
+        System.out.print("\t");                 //Sets up a counter, so that each row and column of
+        for(int i = 0; i < width; i++){         //the resulting print show their respective number.
             System.out.print(i + 1 + "\t");
         }
         System.out.println();
