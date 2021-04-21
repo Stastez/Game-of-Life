6786d6e241a71aa05e6dc152c2f91f72785f9c65
Sandro Steeger <sandro.steeger@student.hpi.de>
2021-04-21 20:56:05 +0200

    Implemented methods to manipulate alive status of cells, implemented countAliveNeighbours and evolve grid.

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -7,12 +7,12 @@ public class Grid {
         }
     }
 
-    Cell[][] grid;
-    int width;
-    int height;
-    int iteration;
+    private Cell[][] grid;
+    private final int width;
+    private final int height;
+    private int iteration;
 
-    Grid(int width, int height){
+    public Grid(int width, int height){
         this.width = width;
         this.height = height;
         makeGrid(width, height);
@@ -23,8 +23,9 @@ public class Grid {
      * Makes a grid from the given dimensions and saves it in the class variable "grid".
      * @param width Describes the x-dimension of the resulting grid. Must be positive.
      * @param height Describes the y-dimension of the resulting grid. Must be positive.
+     * @throws IllegalArgumentException When the arguments are not positive.
      */
-    public void makeGrid(int width, int height){
+    private void makeGrid(int width, int height) throws IllegalArgumentException{
         if(width == 0 | height == 0){
             throw new IllegalArgumentException("width and height always have to be positive.");
         }
@@ -45,7 +46,7 @@ public class Grid {
      * Cells that are alive will be denoted "✓" and cells that are dead will be shown as "🞩".
      */
     public void printGrid(){
-        System.out.println("Iteration " + iteration++ + ": ");
+        System.out.println("Iteration " + iteration++ + ": ");  //Prints the iteration counter header.
         for(int i = 0; i <= width; i++){
             System.out.print("-\t");
         }
@@ -53,21 +54,117 @@ public class Grid {
 
         System.out.print("\t");                 //Sets up a counter, so that each row and column of
         for(int i = 0; i < width; i++){         //the resulting print show their respective number.
-            System.out.print(i + 1 + "\t");
+            System.out.print(i + "\t");
         }
         System.out.println();
-        int counterH = 1;
+        int counterH = 0;
+
+        //int j = 0;
 
         for(int i = 0; i < height; i++){
             System.out.print(counterH++ + "\t");
             for(Cell[] tmp : grid){
-                System.out.print((tmp[i].isAlive ? "✓":"\uD83D\uDFA9") + "\t");
+                System.out.print((tmp[i].isAlive ? "✓":"\uD83D\uDFA9")/*countAliveNeighbours(j++,i)*/ + "\t");
             }
             System.out.println();
+            //j = 0;
         }
     }
 
     public void evolveGrid(){
+        Grid temp = new Grid(width, height);
 
+        for(int x = 0; x < width; x++){
+            for(int y = 0; y < height; y++){
+                switch (countAliveNeighbours(x, y)) {
+                    case 2 -> temp.setStatus(x, y, grid[x][y].isAlive);
+                    case 3 -> temp.setStatus(x, y, true);
+                    default -> temp.setStatus(x, y, false);
+                }
+            }
+        }
+    }
+
+    int countAliveNeighbours(int x, int y) throws IllegalArgumentException{
+        if(x >= width | y >= height | x < 0 | y < 0){
+            throw new IllegalArgumentException("Cannot access position (" + x + "," + y + ") on a grid of dimensions (" + (width - 1) + "," + (height - 1) + ").");
+        }
+
+        int counter = 0;
+
+        if(x >= 1){
+            if(x <= width - 2){
+                if(y >= 1){
+                    if(y <= height - 2){
+                        for(int i = -1; i < 2; i++){
+                            counter+=grid[x+i][y-1].isAlive ? 1:0;
+                            counter+=grid[x+i][y+1].isAlive ? 1:0;
+                        }
+                    }else{
+                        for(int i = -1; i < 2; i++){
+                            counter+=grid[x+i][y-1].isAlive ? 1:0;
+                        }
+                    }
+                }else{
+                    for(int i = -1; i < 2; i++){
+                        counter+=grid[x+i][y+1].isAlive ? 1:0;
+                    }
+                }
+                counter+=grid[x-1][y].isAlive ? 1:0;
+                counter+=grid[x+1][y].isAlive ? 1:0;
+            }else{
+                if(y >= 1){
+                    if(y <= height - 2){
+                        for(int i = -1; i < 1; i++){
+                            counter+=grid[x+i][y-1].isAlive ? 1:0;
+                            counter+=grid[x+i][y+1].isAlive ? 1:0;
+                        }
+                    }else{
+                        for(int i = -1; i < 1; i++){
+                            counter+=grid[x+i][y-1].isAlive ? 1:0;
+                        }
+                    }
+                    counter+=grid[x-1][y].isAlive ? 1:0;
+                }else{
+                    for(int i = -1; i < 1; i++){
+                        counter+=grid[x+i][y+1].isAlive ? 1:0;
+                    }
+                }
+                counter+=grid[x-1][y].isAlive ? 1:0;
+            }
+        }else{
+            if(y >= 1){
+                if(y <= height - 2){
+                    for(int i = 0; i < 2; i++){
+                        counter+=grid[x+i][y-1].isAlive ? 1:0;
+                        counter+=grid[x+i][y+1].isAlive ? 1:0;
+                    }
+                }else{
+                    for(int i = 0; i < 2; i++){
+                        counter+=grid[x+i][y-1].isAlive ? 1:0;
+                    }
+                }
+                counter+=grid[x+1][y].isAlive ? 1:0;
+            }else{
+                for(int i = 0; i < 2; i++){
+                    counter+=grid[x+i][y+1].isAlive ? 1:0;
+                }
+            }
+            counter+=grid[x+1][y].isAlive ? 1:0;
+        }
+
+        return counter;
+    }
+
+    public void kill(int x, int y){
+        grid[x][y].isAlive = false;
+    }
+
+    public void revive(int x, int y){
+        grid[x][y].isAlive = true;
+    }
+
+    public void setStatus(int x, int y, boolean status){
+        grid[x][y].isAlive = status;
     }
 }
